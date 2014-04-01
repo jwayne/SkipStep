e7f329e236515de10448b2552a5152048efa4354
foodle <foodle@dynamic-oit-pu-dormnet-bb-a-201.princeton.edu>
2014-04-01 15:15:08 -0400

    Completed tutorial on storyboards.  Created custom view controllers, did not implement

---
+++ SkipStep/SplashViewController.h
@@ -0,0 +1,15 @@
+//
+//  SplashViewController.h
+//  SkipStep
+//
+//  Created by foodle on 4/1/14.
+//  Copyright (c) 2014 SkipStep. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SplashViewController : UIViewController
+
+- (IBAction)unwindSplash:(UIStoryboardSegue *)segue;
+
+@end
---
+++ SkipStep/StepSequencerViewController.h
@@ -0,0 +1,13 @@
+//
+//  StepSequencerViewController.h
+//  SkipStep
+//
+//  Created by foodle on 4/1/14.
+//  Copyright (c) 2014 SkipStep. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface StepSequencerViewController : UIViewController
+
+@end

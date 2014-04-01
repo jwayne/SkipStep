3f9706a1a17396e6a76d8cb5b1a27dc979e34995
foodle <foodle@dynamic-oit-pu-dormnet-bb-a-201.princeton.edu>
2014-04-01 03:55:25 -0400

    Initial Commit

---
+++ SkipStep/SkipStepAppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  SkipStepAppDelegate.h
+//  SkipStep
+//
+//  Created by foodle on 4/1/14.
+//  Copyright (c) 2014 SkipStep. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SkipStepAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
---
+++ SkipStep/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SkipStep
+//
+//  Created by foodle on 4/1/14.
+//  Copyright (c) 2014 SkipStep. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "SkipStepAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([SkipStepAppDelegate class]));
+    }
+}
